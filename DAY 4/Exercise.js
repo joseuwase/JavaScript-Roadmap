19d52acdef11348f27a6e0b15026260510ac79ec
joseuwase <uwaserubagumyajose@gmail.com>
2026-05-21 20:55:25 +0300

    edit Day 4.js

---
+++ DAY 4/Exercise.js
@@ -0,0 +1,59 @@
+/*let age = 30
+
+if (age >= 18) {
+    console.log("You are not  allowed to age");
+} 
+else {
+    console.log("you are allowed to age");
+}
+
+//Baker' math
+
+
+let sugarweight = 200;
+
+let sugarweight =flourweight + sugarweight;
+
+totalweight /= 3
+
+
+//banking App
+
+let initialbalance = 1000;
+
+initialbalance += 500; // deposit of 500
+initialbalance -= 150;
+initialbalance *= 2;
+console.log("Balance after deposit: " + initialbalance);
+
+// Vip  lounge
+
+let userAge = 21;
+if (userAge >= 21) {
+    console.log(" you are allowed to enter the VIP lounge!");
+} else {
+    console.log("Sorry, you are not allowed to enter the vip lounge .");
+}
+if  (!UserAge >= 18) {
+    console.log("you are allowed to enter the VIP lounge");
+} else {
+    console.log("Sorry, you are not allowed to enter the VIP lounge.");
+}
+
+if (userage !== 13){
+    console.log(" you are not allowed")
+}  
+*/
+
+// E-commerce
+
+let cartTotal = 50;
+let ispremiumMember = true;
+let hasDiscountCode = false;
+let isFraudulent = false;
+
+if (ispremiumMember || cartTotal >= 100) {
+    console.log("You are eligible for free shipping!");
+} else {
+    console.log("Your account is flagged as fraudulent.");
+} 
