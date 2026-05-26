654c54af282e7a3accde6d159f0923c518c19276
joseuwase <uwaserubagumyajose@gmail.com>
2026-05-26 16:17:25 +0300

    edit

---
+++ DAY 7/loops.js
@@ -0,0 +1,21 @@
+
+
+
+
+// we are going to count from 1 to 10
+
+
+for (let p = 5; p >=0; p--) {
+    console.log(p);
+}
+
+
+
+// while loop
+
+let roll = 0
+
+while (roll !== 6) {
+    roll = Math.floor(Math.random() * 6) +1
+    console.log(roll);
+}
