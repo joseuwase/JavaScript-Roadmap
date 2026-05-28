b1b8cfcf97f0a54d59cc3695ce7b9c2c9bd15ec9
joseuwase <uwaserubagumyajose@gmail.com>
2026-05-28 16:13:24 +0300

    edit

---
+++ DAY 8/functions.js
@@ -0,0 +1,17 @@
+
+
+// functions in javascript are a way to encapsulate a block of code that performs a specific task. They can be defined using the function keyword, followed by the function name and parentheses. The code to be executed is enclosed in curly braces. Functions can take parameters, which are values passed to the function when it is called, and they can also return a value.
+// in a function is made up of parameters and arguments
+/*function greet(greeting) {
+    return (greeting + "uwase")
+}
+console.log(greet("hello")
+)*/
+
+let A = 5
+let B = 10
+function add (a,b) {
+    return a + b
+}
+console.log(add(A,B))
+
